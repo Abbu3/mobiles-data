1cfa9956ca2c599a81e398d661ce066730a58fa5
Abbu3 <sa.abbu3@gmail.com>
2024-01-24 16:03:45 +0530

    saved

--- mobile.java
+++ mobile.java
@@ -2,12 +2,10 @@ class mobile
 {
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the 1st number : ");
-		int a = sc.nextInt();
-		System.out.print("Enter the 2nd number : ");
-		int b = sc.nextInt();
-		System.out.println("The value of a and b is " +(a+b));
+		System.out.println("Oneplus");
+		System.out.println("Iphone 15");
+		System.out.println("Realme");
+		System.out.println("Xiaomi");
 	}
 }
-
+
