339df7c8eeeb5ed2592485118d1de98802a25906
Abbu3 <sa.abbu3@gmail.com>
2024-01-24 15:50:23 +0530

    saved

---
+++ mobile.java
@@ -0,0 +1,13 @@
+class mobile 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the 1st number : ");
+		int a = sc.nextInt();
+		System.out.print("Enter the 2nd number : ");
+		int b = sc.nextInt();
+		System.out.println("The value of a and b is " +(a+b));
+	}
+}
+
